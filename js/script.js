04544d0dcb72464e84d8b80494fd7929f0500aaf
broyettore <broyettore@gmail.com>
2023-03-14 19:37:47 +0100

    autoplay added next is hover effect to stop it

--- js/script.js
+++ js/script.js
@@ -10,34 +10,39 @@ const vm = createApp({
         return {
             title: "Spiderman",
             active: 0,
+            activeClass: "thumb active",
             slides: [
                 {
                     image: 'img/01.webp',
                     title: 'Marvel\'s Spiderman Miles Morale',
                     text: 'Experience the rise of Miles Morales as the new hero masters incredible, explosive new powers to become his own Spider-Man.',
+                    toggle: true
                 }, 
                 {
                     image: 'img/02.webp',
                     title: 'Ratchet & Clank: Rift Apart',
                     text: 'Go dimension-hopping with Ratchet and Clank as they take on an evil emperor from another reality.',
+                    toggle: false
                 }, 
                 {
                     image: 'img/03.webp',
                     title: 'Fortnite',
                     text: "Grab all of your friends and drop into Epic Games Fortnite, a massive 100 - player face - off that combines looting, crafting, shootouts and chaos.",
+                    toggle: false
                 }, 
                 {
                     image: 'img/04.webp',
                     title: 'Stray',
                     text: 'Lost, injured and alone, a stray cat must untangle an ancient mystery to escape a long-forgotten city',
+                    toggle: false
                 }, 
                 {
                     image: 'img/05.webp',
                     title: "Marvel's Avengers",
                     text: 'Marvel\'s Avengers is an epic, third-person, action-adventure game that combines an original, cinematic story with single-player and co-operative gameplay.',
+                    toggle: false
                 }
-            ]
-
+            ],
         }
     },
     methods: {
@@ -59,6 +64,22 @@ const vm = createApp({
 
         currentImg(index, event) {
             this.active = index;
-        }
-    }
+        },
+
+        toggleItem (item) {
+            if (item.toggle === true) {
+                item.toggle = false
+            } else {
+                item.toggle = true
+            }
+        },
+
+        // stopPlay() {
+        //     return clearInterval(setInterval(this.nextImg(), 3000));
+        // },
+    }, 
+    mounted() {
+        setInterval(() => this.nextImg(), 3000)
+}
+    
 }).mount("#app")
