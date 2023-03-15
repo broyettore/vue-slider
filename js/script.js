cd852bbc4a8ea9d0baf458b036883d345eaf7363
broyettore <broyettore@gmail.com>
2023-03-15 12:23:58 +0100

    autoplay working now

--- js/script.js
+++ js/script.js
@@ -17,42 +17,44 @@ const vm = createApp({
                     image: 'img/01.webp',
                     title: 'Marvel\'s Spiderman Miles Morale',
                     text: 'Experience the rise of Miles Morales as the new hero masters incredible, explosive new powers to become his own Spider-Man.',
-                    toggle: true
                 }, 
                 {
                     image: 'img/02.webp',
                     title: 'Ratchet & Clank: Rift Apart',
                     text: 'Go dimension-hopping with Ratchet and Clank as they take on an evil emperor from another reality.',
-                    toggle: false
                 }, 
                 {
                     image: 'img/03.webp',
                     title: 'Fortnite',
                     text: "Grab all of your friends and drop into Epic Games Fortnite, a massive 100 - player face - off that combines looting, crafting, shootouts and chaos.",
-                    toggle: false
                 }, 
                 {
                     image: 'img/04.webp',
                     title: 'Stray',
                     text: 'Lost, injured and alone, a stray cat must untangle an ancient mystery to escape a long-forgotten city',
-                    toggle: false
                 }, 
                 {
                     image: 'img/05.webp',
                     title: "Marvel's Avengers",
                     text: 'Marvel\'s Avengers is an epic, third-person, action-adventure game that combines an original, cinematic story with single-player and co-operative gameplay.',
-                    toggle: false
                 }
             ],
         }
     },
     methods: {
+
+        isActive(i) {
+            if (i === this.active) {
+                return true
+            }
+        },
+
         nextImg() {
             this.active++;
 
             if (this.active > this.slides.length - 1) {
-                this.active = 0
-            }
+                this.active = 0;
+            } 
         },
 
         prevImg() {
@@ -67,28 +69,16 @@ const vm = createApp({
             this.active = index;
         },
 
-        toggleItem (item) {
-            if (item.toggle === true) {
-                item.toggle = false
-            } else {
-                item.toggle = true
-            }
-        },
-
         stopInt() {
-            if(this.startInt) {
                 clearInterval(this.setInt);
-            } 
         },
 
         startInt() {
-            if(this.stopInt) {
-                setInterval(this.nextImg, 3000)
-            }
-        },
+                this.setInt = setInterval(() => this.nextImg(), 3000)
+        }
     }, 
     mounted() {
-        this.setInt = setInterval(() => this.nextImg(), 3000)
+         this.startInt()
 }
     
 }).mount("#app")
